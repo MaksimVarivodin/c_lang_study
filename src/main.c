6167b9f835d2b005a45078a298776917cf78e8ea
MaksymVarivodin <maksym.varivodin@gmail.com>
2024-09-03 22:30:18 +0300

    Chapter 1.5.1. Version 1.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#define LOWER 0 /* lower boundary of a table */
-#define UPPER 300 /* upper boundary of a table */
-#define STEP 20 /* step size */
 
 /*
- * Printing table of temperatures for Fahrenheit and Celsius
+ *  Copying input to output
  */
 
-int main ()
+int main(int argc, char* argv[])
 {
-    int fahr;
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-        printf ("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    int c = 0;
+    c = getchar();
+    while (c != EOF)
+    {
+        putchar(c);
+        printf(" ");
+        c = getchar();
+    }
     return 0;
 }
