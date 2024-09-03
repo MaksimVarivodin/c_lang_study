60c68620b44185e17756bf9c40c4a799fee4950d
MaksymVarivodin <maksym.varivodin@gmail.com>
2024-09-03 21:13:52 +0300

    Changed formatting of output.

--- main.c
+++ main.c
@@ -6,16 +6,20 @@
 
 int main()
 {
+    printf(" | %10s | -- | %10s |\n", "", "");
+    printf(" | %10s | => | %10s |\n", "Fahrenheit", "Celsius");
+    printf(" | %10s | -- | %10s |\n", "", "");
     float fahr, celsius;
     int lower, upper, step;
     lower = 0; /* lower boundary for temperature table */
     upper = 300; /* upper boundary */
     step = 20; /* step */
     fahr = lower;
+
     while (fahr <= upper)
     {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%f\t%f\n", fahr, celsius);
+        printf(" | %10.2f | => | %10.2f |\n", fahr, celsius);
         fahr = fahr + step;
     }
 
