b938016a0c0b5430042f6821b90879844199a52b
MaksymVarivodin <maksym.varivodin@gmail.com>
2024-09-03 21:18:53 +0300

    Using 'for' cycle.

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main()
     printf(" | %10s | -- | %10s |\n", "", "");
     printf(" | %10s | => | %10s |\n", "Fahrenheit", "Celsius");
     printf(" | %10s | -- | %10s |\n", "", "");
-    int fahr{0};
+    int fahr = 0;
     for (; fahr <= 300; fahr = fahr + 20)
-        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+        printf(" | %10d | => | %10.1f |\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
     return 0;
 }
