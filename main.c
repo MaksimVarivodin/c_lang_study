8bc5d1ed84b482bba9e7e79862eb278ba8a0d77d
MaksymVarivodin <maksym.varivodin@gmail.com>
2024-09-03 20:58:21 +0300

    Chapter 1.2

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(void)
+/*
+ * Printing temperature table by Fahrenheit and Celsius
+ * for "fahr" from 0 to 300
+ */
+
+int main()
 {
-    printf("Hello, World!\n");
+    int fahr, celsius;
+    int lower, upper, step;
+    lower = 0; /* lower boundary for temperature table */
+    upper = 300; /* upper boundary */
+    step = 20; /* step */
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr-32) / 9;
+        printf ("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
     return 0;
 }
