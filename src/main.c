6336e5a08e83c302a4d0431c5a3501bb5e602401
MaksymVarivodin <maksym.varivodin@gmail.com>
2024-09-09 23:54:43 +0300

    Chapter 1.5.4.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
 /*
- *  Counting entered lines
+ *  counting lines, words and symbols
  */
+
+#define IN 1 /* state in */
+#define OUT 0 /* state out of word */
 
 int main()
 {
-    int c, nl;
-    nl = 0;
-    while ((c = getchar()) != EOF)
-        if (c == '\n')
-            ++nl;
-    printf ("%d\n", nl);
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n' )++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf ("%d %d %d\n", nl, nw, nc);
     return 0;
 }
